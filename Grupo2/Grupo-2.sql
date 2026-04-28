e4d812d42aed02994f9775959644641bca624510
jcsalamanca-uam <169182487+jcsalamanca-uam@users.noreply.github.com>
2026-04-28 15:57:44 -0600

    Delete Grupo2 directory

--- Grupo2/Grupo-2.sql
+++
@@ -1,9 +0,0 @@
-USE TechStore;
-GO
--- Verificar la existencia de las tablas principales
-SELECT TABLE_NAME, TABLE_TYPE 
-FROM INFORMATION_SCHEMA.TABLES;
-GO
--- Consultar los datos insertados en una de las tablas
-SELECT * FROM Productos;
-GO
